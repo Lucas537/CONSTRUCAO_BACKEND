23a4b234573b22d8e03a20ce73f610ef22f6a862
Lucas537 <ll6767248@gmail.com>
2025-09-08 21:09:02 -0300

    04

---
+++ Praticas/04-calculadora-notas/index.js
@@ -0,0 +1,23 @@
+
+// Importar o Express
+const express = require ("express")
+
+// Cria uma instância do application com express
+const app = express()
+
+
+
+// Configuração e Intermediários (Middlawares)
+
+
+
+
+
+
+// Executo a aplicação (Minha API)
+app.listen(3000, () => {
+console.log ("Api rodando em http://localhost:3000")
+
+})
+
+
